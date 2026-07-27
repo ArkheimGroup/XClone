239cf037e5615c0566a4c80cb0ad40493387d908
Farnam Jahangard <farnamjahangard@gmail.com>
2026-07-27 17:45:12 +0330

    feat: add media and profile picture support to client

--- server/src/main/java/arkheim/server/application/dtos/responses/UserResponse.java
+++ server/src/main/java/arkheim/server/application/dtos/responses/UserResponse.java
@@ -11,21 +11,28 @@ public record UserResponse(
         UUID id,
         String username,
         String email,
-        String name
+        String name,
+        String pfpUrl
 ) {
     public UserResponse(User user){
         this(
                 user.getId(),
                 user.getUsername(),
                 user.getEmail(),
-                user.getName()
+                user.getName(),
+                user.getPfpUrl()
         );
     }
 
     public UserResponse(UUID id, String username, String email, String name) {
+        this(id, username, email, name, "uploads/profile_pictures/default_pfp.png");
+    }
+
+    public UserResponse(UUID id, String username, String email, String name, String pfpUrl) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.name = name;
+        this.pfpUrl = pfpUrl;
     }
 }
