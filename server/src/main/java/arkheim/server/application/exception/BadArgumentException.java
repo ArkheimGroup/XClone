82556827bfeb6d7f5f39d24808b35a9b1f8d4bd8
Ramtin Jafari <ramtinjafari86@gmail.com>
2026-07-31 11:38:14 +0330

    feat: remove past misplaced exceptions

--- server/src/main/java/arkheim/server/application/exception/BadArgumentException.java
+++
@@ -1,7 +0,0 @@
-package arkheim.server.application.exception;
-
-public class BadArgumentException extends BaseApplicationException {
-    public BadArgumentException(ErrorCode code, String message) {
-        super(code, message);
-    }
-}
